c0a2fee8932a103842bc2bea351d4f9fcf6d0b36
KellieLittlejohn <big_zaddy@Bigs-MacBook-Pro.local>
2022-10-07 00:25:06 -0400

    commit task 4

--- starter/03check-errors /checkErrorsTest.test.ts
+++ starter/03check-errors /checkErrorsTest.test.ts
@@ -10,7 +10,7 @@ it('TODO: task 3 goes here', () => {
   
     function errCheck(emptyArr){
       if (emptyArr.length === 0) {
-        let emptyArrError = 'The array is empty'
+        let emptyArrError = 'Empty array'
         return emptyArrError
 
     // Assert
@@ -18,13 +18,20 @@ it('TODO: task 3 goes here', () => {
     expect(errCheck).toThrow(emptyArrError)
   }
   }
-  
+
   })
   
   it('TODO: task 4 goes here', () => {
     // Arrange and Act
     // create a function that calls the add function without any arguments and assign it to a variable
+
+    function add2(param){
+      if (param === 0) {
+        let emptyAddError = 'Nothing to add'
+        return emptyAddError
+        
   
     // Assert
     // use the expect function to assert that the function throws an error, use toThrow function
-  })
+
+    expect(add2).toThrowError(emptyAddError)
