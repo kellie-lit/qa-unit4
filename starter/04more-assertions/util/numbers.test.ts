e86a79060a7bda480e9275a275e6fb2f33082a40
KellieLittlejohn <big_zaddy@Bigs-MacBook-Pro.local>
2022-10-07 00:33:11 -0400

    test failed cannot figure out error

--- starter/04more-assertions/util/numbers.test.ts
+++ starter/04more-assertions/util/numbers.test.ts
@@ -12,11 +12,18 @@ it('TODO: Task 3 goes here', () => {
     // Arrange
     // create a variable with a string value that is a valid number, e.g. '1'
 
+    let nine = '9'
+
+
     // Act
     // call the transformToNumber function on an input value and assign the result to a variable
 
+    let result = transformToNumber(nine)
+
     // Assert
     // use the expect function to assert that the result is equal to the expected result
+
+    expect(result).toBe(9)
 })
 
 it('TODO: Task 4 goes here', () => {
@@ -27,6 +34,13 @@ it('TODO: Task 4 goes here', () => {
     // Act
     // for each input value, call the transformToNumber function on an input value and assign the result to a variable
 
+    let firstInput = transformToNumber(input)
+
+    let secondInput = transformToNumber(input2)
+
     // Assert
     // for each result value, use the expect function to assert that the result is equal to NaN, use toBeNaN function
+
+    expect(firstInput).toBeNaN()
+    expect(secondInput).toBeNaN()
 })
